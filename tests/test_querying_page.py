ae762396599ec3712832b061162128058f51e02f
ShibaevSergey <nerik20@yandex.ru>
2024-05-05 17:06:19 +0300

    Добавил тесты для страницы Querying

--- tests/test_querying_page.py
+++ tests/test_querying_page.py
@@ -1,4 +1,26 @@
+
+
+
 def test_button(querying_page):
     querying_page.navigate()
     querying_page.click_btn()
     querying_page.expect_btn_name()
+
+def test_div(querying_page):
+    querying_page.navigate()
+    querying_page.get_class_from_data_test_id()
+
+def test_get_element_from_text(querying_page):
+    querying_page.navigate()
+    querying_page.expect_class_elements_from_text()
+    querying_page.expect_class_element_from_role()
+
+def test_fill_query_form(querying_page):
+    querying_page.navigate()
+    querying_page.input_name()
+    querying_page.input_email()
+    querying_page.input_password()
+
+def test_click_btn_submit(querying_page):
+    querying_page.navigate()
+    querying_page.click_btn_submit()
